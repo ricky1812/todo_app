f20bfcac3f09d4d61368e4a87b6752bea49de133
ricky1812 <rickydas189@gmail.com>
2019-05-29 09:57:58 +0530

    added js

--- static/script.js
+++ static/script.js
@@ -1,13 +1,31 @@
+var note=document.getElementById('notifications');
+var db;
+var newItem = [{ task: "", hours: 0, minutes: 0, day: 0, month: "", year: 0}];
+var tasklist=document.getElementById('task-list');
+var task=document.getElementById('task');
+var hours=document.getElementById('deadline-hour');
+var minutes=document.getElementById('deadline-minute');
+var seconds=document.getElementById('deadline-second');
+var day = document.getElementById('deadline-day');
+var month = document.getElementById('deadline-month');
+var year = document.getElementById('deadline-year');
 
-document.getElementById("add").addEventListener('click',function(){
-	var task=document.getElementById('task');
-	var date=document.getElementById('datetime');
-	console.log(date);
-	console.log(task);
-	localStorage.setItem('task',task);
-	localStorage.setItem('datetime',date);
 
-	alert(task);
-	
-})
+function checkdeadlines()
+{
+	var now=new Date();
+	var minuteCheck = now.getMinutes();
+    var hourCheck = now.getHours();
+    var dayCheck = now.getDate();
+    var monthCheck = now.getMonth();
+    var yearCheck = now.getFullYear();
+}
 
+
+function notification()
+{
+	var text = 'HEY! Your task "' + task + '" needs to be done".';
+	alert(text);
+}
+
+setInterval(checkDeadlines, 1000);
